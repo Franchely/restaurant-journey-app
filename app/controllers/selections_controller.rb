3f854c03c2ac2079d6e7f72e1fc530a4e512d229
github username <franchely811@gmail.com>
2019-08-16 16:04:13 -0400

    Adds routes controllers

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,2 +1,29 @@
 class SelectionsController < ApplicationController
+
+    def index 
+        selections = Selection.all 
+        render json: selections 
+    end
+
+    def show
+        selection = Selection.find(params[:id])
+        render json: selection
+    end
+
+    def new
+        selection = Selection.new
+        render json: selection
+    end
+
+    def create
+        selection = Selection.create(selection_params)
+        render json: selection
+    end
+
+    private
+
+    def selection_params
+        params.require(:selection).permit(:user_id, :restaurant_id, :score)
+    end
+
 end
