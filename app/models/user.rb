f12879a87a842eaa50d1c43483aeee7d89b2f0fd
Kevin Wang <kcwang06@gmail.com>
2020-01-06 18:05:43 -0500

    first heroku commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_many :selections
-    has_many :restaurants, through: :selections
-    
-    has_secure_password
+    has_many :restaurants, through: :selections 
+    # has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
 end
